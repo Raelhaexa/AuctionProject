c9dc76391af477ece91fbba4b7bd846d53195a63
John Wendell Senados <WendellSen28@gmail.com>
2025-12-10 13:52:56 +0800

    Created Ui for Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-import LoginPage from './pages/LoginPage.jsx';
-import RegisterPage from './pages/RegisterPage.jsx';
+// import DashboardPage from './pages/Dashboard/DashboardPage.jsx';
+import LoginPage from './pages/Login/LoginPage.jsx';
+// import RegisterPage from './pages/Register/RegisterPage.jsx';
 import { Route,Routes } from 'react-router-dom';
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
 
    <Routes>
       <Route path='/' element={<LoginPage/>} />
-      <Route path='/register' element={<RegisterPage/>} />
+      {/* <Route path='/register' element={<RegisterPage/>} />
+      <Route path='/dashboard' element={<DashboardPage/>} /> */}
    </Routes>
   )
 }
